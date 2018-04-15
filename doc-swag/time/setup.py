b3db00ccbfb254ff156ca0fc083aa88a1a7245ec
karanrk <karankamatgi@gmail.com>
2018-04-15 02:21:21 -0400

    timestamp stub moved

---
+++ doc-swag/time/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='time', version='1.0', packages=[''], url='', license='', author='Karan Kamatgi',
+    author_email='krkamatg.iu.edu',
+    description='This is a package for some time functionalities to be used in the REST service in '
+                'the course Cloud Computing.')
